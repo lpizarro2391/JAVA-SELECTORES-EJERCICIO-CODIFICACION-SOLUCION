9567d39bf4acbcb6a02cede74553858bb88e032a
lpizarro <lpizarro2391@github.com>
2020-05-24 20:30:09 -0600

    se debe ingresar en lla consola  document.onkeypress=mostrar_tecla; para que se muestre la techa

--- js_eventos_teclado/js/eventos.js
+++ js_eventos_teclado/js/eventos.js
@@ -1,1 +1,5 @@
-
+function mostrar_tecla(event){
+    var tecla = event.which || event.keyCode;
+    document.querySelector("#tarjeta_muestra_tecla #texto_tarjeta")
+        .innerHTML="Presionaste la tecla: "+String.fromCharCode(tecla);
+}
