7b98988f92ddd879705f9238bc11c16debcc0a8f
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-25 15:47:37 -0600

    funcion cambio foto extraterrestre, aplica un efecto de desnfoque a la imagen del extraterrestre

--- js_eventos_mouse/js/eventos.js
+++ js_eventos_mouse/js/eventos.js
@@ -2,3 +2,7 @@ function cambioFotoAstronauta(){
     document.getElementById('Astronauta').style="width:50%"
 }
 
+
+function cambioFotoExtraterrestre(){
+    document.getElementById('Extraterrestre').style="webkit-filter:blur(5px);filter:blur(5px);"
+}
