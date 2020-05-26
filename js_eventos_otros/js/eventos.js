fdc21293d5d167b2414d39a3559e2ca2a85d3e3a
lpizarro <lpizarro2391@github.com>
2020-05-25 20:34:31 -0600

    efectos onblur, onfocus

--- js_eventos_otros/js/eventos.js
+++ js_eventos_otros/js/eventos.js
@@ -18,3 +18,56 @@ function cargarPagina(){
 
 document.getElementsByTagName('body')[0].onload = cargarPagina;
 
+
+function eventoOnFocus(event) {
+    var seleccion = event.currentTarget.id
+    switch (seleccion) {
+        case "animales":
+            document.getElementById('sel_animales').disable = false
+            $(document).ready(function () { $('select').material_select() })
+            break
+        case "postres":
+            document.getElementById('sel_postres').disable = false
+            $(document).ready(function () { $('select').material_select() })
+            break
+        case "musica":
+            document.getElementById('sel_musica').disable = false
+            $(document).ready(function () { $('select').material_select() })
+            break;
+        default:
+    }
+}
+
+
+function eventoOnBlur(event) {
+    var seleccion = event.currentTarget.id;
+    switch (seleccion) {
+        case "animales":
+            document.getElementById('sel_animales').disable = true
+            $(document).ready(function () { $('select').material_select() })
+            break
+        case "postres":
+            document.getElementById('sel_postres').disable = true
+            $(document).ready(function () { $('select').material_select() })
+            break
+        case "musica":
+            document.getElementById('sel_musica').disable = true
+            $(document).ready(function () { $('select').material_select() })
+            break
+        default:
+    }
+}
+
+
+function asignarEventoRB() {
+    document.getElementById('animales').onfocus = evento0nFocus
+    document.getElementById('postres').onfocus = evento0nFocus
+    document.getElementById('musica').onfocus = evento0nFocus
+
+    document.getElementById('animales').onblur = evento0nFocus
+    document.getElementById('postres').onblur = evento0nFocus
+    document.getElementById('musica').onblur = evento0nFocus
+
+}
+
+asignarEventoRB()
